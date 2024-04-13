28b4656ce8d8fcac40d00c1899f6e8f6498831de
Alvinyw <alvin.yiwen@gmail.com>
2024-04-13 15:14:16 +0800

    新增 图片压缩和图片文本搜索

---
+++ src/views/imgCompress/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="img-compress">
+
+    </div>
+</template>
+<script>
+
+export default {
+    name: "ImgCompress",
+    data() {
+        return {
+        };
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
+<style lang="scss" scoped>
+.img-compress {}
+</style>
---
+++ src/views/imgTextSearch/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="img-text-search">
+        暂时没时间搞了，有使用需求的话可以联系总行/科技部/廖益文。
+    </div>
+</template>
+<script>
+
+export default {
+    name: "ImgTextSearch",
+    data() {
+        return {
+        };
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
+<style lang="scss" scoped>
+.img-text-search {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+}
+</style>
