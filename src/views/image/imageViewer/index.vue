dce30523b995320dc5b047f0bc4a820a6c5c3453
Alvinyw <alvin.yiwen@gmail.com>
2024-04-27 11:07:15 +0800

    调整图片目录

---
+++ src/views/image/imageViewer/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="image-viewer">
+    <div class="btn-grp">
+      <el-button type="primary" size="small" @click="fuc_LoadImage">导入图片</el-button>
+      <!-- <el-button type="primary" size="small" @click="fuc_showVideo">show video</el-button> -->
+      <el-button type="primary" size="small" @click="fuc_ShowImageEditor">编辑</el-button>
+      <el-button type="primary" size="small" @click="fuc_getImage">下载选中图片</el-button>
+      <el-button type="primary" size="small" @click="fuc_RemoveCurrentImage">删除选中图片</el-button>
+      <el-button type="primary" size="small" @click="fuc_RemoveAllImages">删除所有图片</el-button>
+      <div class="btn-grp-edit" v-show="isEditing">
+        <el-button type="primary" size="small" @click="fuc_rotateLeft">向左旋转</el-button>
+        <el-button type="primary" size="small" @click="fuc_rotateRight">向右旋转</el-button>
+        <!-- <el-button type="primary" size="small" @click="fuc_rotate180">Rotate180</el-button> -->
+        <el-button type="primary" size="small" @click="fuc_rotateMirror">镜像翻转</el-button>
+        <el-button type="primary" size="small" @click="fuc_rotateFlip">上下翻转</el-button>
+        <el-button type="primary" size="small" @click="fuc_Crop">裁切</el-button>
+        <el-button type="primary" size="small" @click="fuc_Save">确定</el-button>
+        <el-button type="primary" size="small" @click="fuc_CloseImageEditor">取消</el-button>
+      </div>
+
+    </div>
+    <div id="imageViewer"></div>
+  </div>
+</template>
+<script>
+import createImageViewer from "alvin-imageviewer";
+export default {
+  name: "ImageViewer",
+  data() {
+    return {
+      imageViewer: "",
+      isEditing: false,
+    };
+  },
+  mounted() {
+    let Width = document.body.clientWidth - 230 + 'px'
+    let Height = document.body.clientHeight - 170 + 'px'
+    var cfg = {
+      ContainerId: "imageViewer",
+      Width,
+      Height,
+    };
+    this.imageViewer = createImageViewer(cfg);
+  },
+  methods: {
+    globalCheck() {
+      if (!this.imageViewer) {
+        return false;
+      }
+      var _imageCount = this.imageViewer.GetCount();
+      if (_imageCount < 1) {
+        this.$message({
+          message: '请先导入图片',
+          type: 'warning'
+        });
+        return false;
+      }
+      return true;
+    },
+    fuc_LoadImage() {
+      if (!this.imageViewer) {
+        return;
+      }
+      this.imageViewer.LoadImageEx();
+    },
+    fuc_showVideo() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.ShowVideo();
+    },
+    fuc_ShowImageEditor() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.ShowImageEditor();
+      this.isEditing = true;
+    },
+    fuc_CloseImageEditor() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.CloseImageEditor();
+      this.isEditing = false;
+    },
+    fuc_rotateLeft() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.RotateLeft();
+    },
+    fuc_rotateRight() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.RotateRight();
+    },
+    fuc_rotate180() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.Rotate('', 180);
+    },
+    fuc_rotateMirror() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.Mirror();
+    },
+    fuc_rotateFlip() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.Flip();
+    },
+    fuc_Crop() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.Crop();
+    },
+    fuc_Save() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.Save();
+      this.isEditing = false;
+    },
+    fuc_RemoveCurrentImage() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.RemoveAllSelectedImages();
+    },
+    fuc_RemoveAllImages() {
+      if (!this.globalCheck()) return;
+      this.imageViewer.RemoveAllImages();
+    },
+    fuc_getImage() {
+      if (!this.globalCheck()) return;
+      var _curIndex = this.imageViewer.GetCurentIndex();
+      this.imageViewer.SaveAsPNG('image' + _curIndex, _curIndex);
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+.image-viewer {
+  .btn-grp {
+    margin: 0 0 10px;
+
+    .btn-grp-edit {
+      margin: 10px 0 0;
+    }
+  }
+}
+</style>
---
+++ src/views/image/imgCompress/index.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="img-compress">
+        <div class="sec-lt">
+            <input id="imageUpload" type="file" accept="image/png, image/jpeg, image/gif, image/jpg, image/svg+xml"
+                @change="onUploadChange" />
+            <p class="lable">原图大小： {{ $lib.calcBase64Size(base64File) }}M:</p>
+            <div id="orignImage">
+                <img v-show="base64File" style="max-width: 95%;" :src="base64File" alt="原图" />
+            </div>
+        </div>
+        <div class="sec-rt">
+            压缩比例：
+            <el-input v-model="sizeValue" placeholder="请输入压缩比例" style="width: 100px; margin: 0 15px 0 0;"></el-input>
+            <el-button type="primary" @click="compressImg" :loading="isLoading">压缩</el-button>
+            <el-button type="primary" @click="downloadImg">下载</el-button>
+            <p class="lable">压缩后图片大小：{{ $lib.calcBase64Size(newBase64File) }}M</p>
+            <div id="resultImage">
+                <img v-show="newBase64File" style="max-width: 95%;" :src="newBase64File" alt="原图" />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "ImgCompress",
+    data() {
+        return {
+            imageFile: '',
+            base64File: '',
+            newBase64File: '',
+            sizeValue: '1',
+            isLoading: false,
+        };
+    },
+    mounted() {
+    },
+    methods: {
+        onUploadChange() {
+            var file = document.getElementById("imageUpload").files[0];
+            //判断获取的是否为图片文件
+            if (!/image\/\w+/.test(file.type)) {
+                this.$message({
+                    message: '请确保导入文件为图片',
+                    type: 'warning'
+                });
+                return false;
+            }
+            this.imageFile = file;
+            // 转成 base64
+            //判断浏览器是否支持filereader
+            if (typeof FileReader == undefined) {
+                this.$message({
+                    message: '抱歉，你的浏览器版本较低，不支持 FileReader',
+                    type: 'warning'
+                });
+                return false;
+            }
+            var reader = new FileReader();
+            const sele = this;
+            reader.readAsDataURL(this.imageFile);
+            reader.onload = function () {
+                sele.base64File = reader.result;
+            }
+        },
+        compressImg() {
+            this.isLoading = true;
+            this.newBase64File = '';
+            this.$lib.compressionImage(this.base64File, newSrc => {
+                this.newBase64File = newSrc;
+                this.isLoading = false;
+            }, this.$lib.calcBase64Size(this.base64File) * this.sizeValue);
+        },
+        downloadImg() {
+            var _this = this;
+            this.$lib.getBlobFromAnyImgData(_this.newBase64File, (blob) => {
+                var blobFile = blob;
+                if (blob instanceof Blob) {
+                    blobFile = URL.createObjectURL(blob);
+                }
+                var a = document.createElement('a');
+                a.target = '_blank';
+                a.download = (new Date()).getTime() + '.png';
+                var objUrl = blobFile;
+                a.href = objUrl;
+                var ev = new MouseEvent('click', {
+                    "view": window,
+                    "bubbles": true,
+                    "cancelable": false
+                });
+                a.dispatchEvent(ev);
+                //a.click();
+                setTimeout(function () {
+                    URL.revokeObjectURL(objUrl);
+                }, 10000);
+            });
+        },
+        handleCopy() {
+            var input = document.getElementById("copyInput");
+            input.value = this.base64File;
+            input.select();//选中文本
+            document.execCommand("copy");
+            this.$message({
+                message: '已拷贝 base64 文本！',
+                type: 'success'
+            });
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.img-compress {
+    display: flex;
+    justify-content: space-between;
+
+    .lable {
+        margin: 10px 0;
+    }
+
+    .sec-lt {
+        width: 50%;
+        min-height: 500px;
+        border-right: 1px solid #ddd;
+
+        #imageUpload {
+
+            &::file-selector-button {
+                display: inline-block;
+                line-height: 1;
+                cursor: pointer;
+                border: 1px solid #409eff;
+                color: #fff;
+                background-color: #409eff;
+                text-align: center;
+                -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+                outline: 0;
+                -webkit-transition: .1s;
+                transition: .1s;
+                font-weight: 500;
+                padding: 12px 20px;
+                font-size: 14px;
+                border-radius: 4px;
+            }
+        }
+    }
+
+    .sec-rt {
+        padding: 0 0 0 15px;
+        width: 50%;
+
+        .base64-wrapper {
+            padding: 5px;
+            margin: 10px 0 0;
+            height: 120px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            cursor: pointer;
+            word-wrap: break-word;
+            word-break: normal;
+            overflow-y: scroll;
+        }
+    }
+}
+</style>
