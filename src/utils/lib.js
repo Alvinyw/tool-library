1f19f7dfb1bd1c7bfdffb9a1e9a101c0c075fbad
Alvinyw <alvin.yiwen@gmail.com>
2024-11-12 16:11:02 +0800

    抽离图片转base64方法

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -386,6 +386,31 @@ export function downloadPdf(blobFile = '', type = 'PNG', width = 0, height = 0) 
 }
 
 /**
+ * 将base64格式的图片下载为txt文件
+ * @param { base64File } base64格式的图片源
+ * */
+export function downloadBase64ToTxt(base64File = '') {
+	// dada 表示要转换的字符串数据，type 表示要转换的数据格式
+	const blob = new Blob([base64File], {
+		type: "text/plain;charset=utf-8"
+	})
+	// 根据 blob生成 url链接
+	const objectURL = URL.createObjectURL(blob)
+
+	// 创建一个 a 标签Tag
+	const aTag = document.createElement('a')
+	// 设置文件的下载地址
+	aTag.href = objectURL
+	// 设置保存后的文件名称
+	aTag.download = `${(new Date()).getTime()}.txt`;
+	// 给 a 标签添加点击事件
+	aTag.click()
+	// 释放一个之前已经存在的、通过调用 URL.createObjectURL() 创建的 URL 对象。
+	// 当你结束使用某个 URL 对象之后，应该通过调用这个方法来让浏览器知道不用在内存中继续保留对这个文件的引用了。
+	URL.revokeObjectURL(objectURL)
+}
+
+/**
  * 计算base64格式图片的大小，单位M
  * @param { base64 } base64格式的图片源
  * @returns 返回传入的base64图片的大小
--- src/views/image/imgFormatConvert/index.vue
+++ src/views/image/imgFormatConvert/index.vue
@@ -149,7 +149,7 @@ export default {
         // 图片下载
         downloadImg() {
             if (Number(this.currentValue) == 0) {
-                return this.downloadBase64ToTxt()
+                return this.$lib.downloadBase64ToTxt(this.base64File)
             }
             if (Number(this.currentValue) == 4) {
                 const { type = 'PNG', width = 0, height = 0 } = this.imgOriginalInfo;
@@ -172,27 +172,6 @@ export default {
                 URL.revokeObjectURL(objUrl);
             }, 10000);
 
-        },
-        // 下载 bas64 文本
-        downloadBase64ToTxt() {
-            // dada 表示要转换的字符串数据，type 表示要转换的数据格式
-            const blob = new Blob([this.base64File], {
-                type: "text/plain;charset=utf-8"
-            })
-            // 根据 blob生成 url链接
-            const objectURL = URL.createObjectURL(blob)
-
-            // 创建一个 a 标签Tag
-            const aTag = document.createElement('a')
-            // 设置文件的下载地址
-            aTag.href = objectURL
-            // 设置保存后的文件名称
-            aTag.download = `${(new Date()).getTime()}.txt`;
-            // 给 a 标签添加点击事件
-            aTag.click()
-            // 释放一个之前已经存在的、通过调用 URL.createObjectURL() 创建的 URL 对象。
-            // 当你结束使用某个 URL 对象之后，应该通过调用这个方法来让浏览器知道不用在内存中继续保留对这个文件的引用了。
-            URL.revokeObjectURL(objectURL)
         },
         // 复制 base64 文本
         handleCopy() {
